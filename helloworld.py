781b529f4866fa3210a34568d892d95e9084499c
pansong291 <pansong291@sina.com>
2025-02-19 16:30:47 +0800

    增加一个输出字符图形的函数

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,11 @@
 print("hello world")
 print(1 + 2)
 print(True)
+
+def output_graph(x: int):
+    for i in range(x):
+        print("*", end="")
+
+if __name__ == "__main__":
+    output_graph(2)
+    output_graph(3)
