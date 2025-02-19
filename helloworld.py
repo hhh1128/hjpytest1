add970217a0ee8e874a63d9f64e18f2ba859e8eb
pansong291 <pansong291@sina.com>
2025-02-19 16:48:23 +0800

    增加一个参数可指定打印的字符

--- helloworld.py
+++ helloworld.py
@@ -3,10 +3,10 @@ print("hello world")
 print(1 + 2)
 print(True)
 
-def output_graph(x: int):
+def output_graph(x: int, c: str):
     for i in range(x):
-        print("*", end="")
+        print(c, end="")
 
 if __name__ == "__main__":
-    output_graph(2)
-    output_graph(3)
+    output_graph(2, "*")
+    output_graph(3, "+")
