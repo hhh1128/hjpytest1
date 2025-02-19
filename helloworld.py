76347f463eca0e202e7521abca7aef10b61a4b11
huihui <2233841700@qq.com>
2025-02-19 17:41:34 +0800

    格式化代码

--- helloworld.py
+++ helloworld.py
@@ -6,10 +6,10 @@ print(True)
 def output_graph(x: int, y:int, c: str):
     for i in range(x):
         for j in range(y):
-            print(c,end="")
+            print(c, end="")
         print("", end="\n")
 
 
 if __name__ == "__main__":
     output_graph(2,3,"*")
-    output_graph(2,3,"+")
+    output_graph(2, 3, "+")
