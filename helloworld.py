c845c8da8f12e26ee6c5474b528571b437e5a22a
huihui <2233841700@qq.com>
2025-02-19 16:57:23 +0800

    增加一个参数y，使其打印一个 x 行，y 列的星号矩形

--- helloworld.py
+++ helloworld.py
@@ -3,10 +3,13 @@ print("hello world")
 print(1 + 2)
 print(True)
 
-def output_graph(x: int):
+def output_graph(x: int,y:int):
     for i in range(x):
-        print("*", end="")
+        for i in range(y):
+            print("*",end="")
+        print("", end="\n")
+
 
 if __name__ == "__main__":
-    output_graph(2)
-    output_graph(3)
+    output_graph(2,3)
+
